702ee637434a75e6fea418229fe845daf4ed7a66
Roam-Payos <osato.izegbuwa@codebytesltd.com>
2026-04-21 14:11:24 +0100

    Add app/gift-card/_layout.tsx

---
+++ app/gift-card/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function GiftCardLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="new" />
+      <Stack.Screen name="[id]" />
+    </Stack>
+  );
+}
